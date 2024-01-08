293a2b7b48df8ea6f631c254c477c1c293b750c9
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-08 23:51:10 +0530

    Added controllers and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { connect } from './config/database.js'; // Adjust the path to your database module
 import { PORT } from './config/serverConfig.js'; // Adjust the path to your serverConfig module
 
+import apiRoutes from './routes/index.js';
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
 
 app.listen(PORT, async () => {
     console.log(`Server Started on ${PORT}`);
