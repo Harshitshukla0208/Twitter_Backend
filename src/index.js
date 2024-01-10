c9b9b91cc7d74ae5ae5f138d94be6d4522887316
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-10 19:22:03 +0530

    completed comments api

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import { PORT } from './config/serverConfig.js'
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
-import { UserRepository, TweetRepository } from './repository/index.js';
-import LikeService from './services/like-service.js';
 
 const app = express();
 
-// Middleware for parsing JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
 app.use('/api', apiRoutes);
 
-// Start the server and connect to MongoDB
-app.listen(3000, async () => {
-    console.log('Server started');
+app.listen(PORT, async () => {
+    console.log(`Server started on port ${PORT}`);
     
-    // Connect to MongoDB
     await connect();
     console.log('MongoDB connected');
-
-    // Fetch data from repositories
-    // const userRepository = new UserRepository();
-    // const tweetRepository = new TweetRepository();
-    // const tweets = await tweetRepository.getAll(0, 10);
-    // const users = await userRepository.getAll();
-
-    // // Example usage of LikeService
-    // const likeService = new LikeService();
-    // await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
 });
 
 export default app;
